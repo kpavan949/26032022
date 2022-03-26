67bb3c4942b3d1d209e6a1ea08fa1cc1b895ed6c
root <root@ip-172-31-2-221.ap-south-1.compute.internal>
2022-03-26 16:19:44 +0000

    some massage

---
+++ src/main/java/2157600/App.java
@@ -0,0 +1,13 @@
+package 2157600;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
